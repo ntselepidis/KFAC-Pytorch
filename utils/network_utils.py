e06b4830951cc4387201f9d357504ada9b13e11b
ntselepidis <ntselepidis@student.ethz.ch>
2020-05-13 00:07:58 +0200

    Added support for all VGG variants.

--- utils/network_utils.py
+++ utils/network_utils.py
@@ -1,18 +1,23 @@
-from models.cifar import (alexnet, densenet, resnet,
-                          vgg16_bn, vgg19_bn,
-                          wrn, simple_cnn)
-
+from models.cifar import (alexnet, densenet, resnet, wrn,
+                          vgg11, vgg11_bn, vgg13, vgg13_bn,
+                          vgg16, vgg16_bn, vgg19_bn, vgg19,
+                          simple_cnn)
 
 def get_network(network, **kwargs):
     networks = {
         'alexnet': alexnet,
         'densenet': densenet,
         'resnet': resnet,
+        'wrn': wrn,
+        'vgg11': vgg11,
+        'vgg13': vgg13,
+        'vgg16': vgg16,
+        'vgg19': vgg19,
+        'vgg11_bn': vgg11_bn,
+        'vgg13_bn': vgg13_bn,
         'vgg16_bn': vgg16_bn,
         'vgg19_bn': vgg19_bn,
-        'wrn': wrn,
         'simple_cnn': simple_cnn
-
     }
 
     return networks[network](**kwargs)
