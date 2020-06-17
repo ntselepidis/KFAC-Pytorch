f106fa51935e8cadcc68df353566c00d773cc0f0
ntselepidis <ntselepidis@student.ethz.ch>
2020-06-17 02:12:21 +0200

    Modified logging.

--- main.py
+++ main.py
@@ -140,9 +140,39 @@ if args.depth == 0:
 else:
     args_depth = str(args.depth)
 
-log_dir = os.path.join(args.log_dir, args.dataset, args.network + args_depth, args.optimizer,
-                       'bs%d_lr%.4f_mom%.4f_wd%.4f_dmp%.4f' %
-                       (args.batch_size, args.learning_rate, args.momentum, args.weight_decay, args.damping))
+if optim_name == 'sgd' or optim_name == 'adam':
+    log_dir = os.path.join(args.log_dir, args.dataset, args.network + args_depth, args.optimizer,
+                           'bs%d_lr%.2f_mom%.2f_wd%.4f' %
+                           (args.batch_size,
+                            args.learning_rate,
+                            args.momentum,
+                            args.weight_decay))
+elif optim_name == 'kfac':
+    log_dir = os.path.join(args.log_dir, args.dataset, args.network + args_depth, args.optimizer,
+                           'bs%d_lr%.2f_mom%.2f_wd%.4f_sd%.4f_dmp%.4f_kl%.4f_TCov%d_TInv%d' %
+                           (args.batch_size,
+                            args.learning_rate,
+                            args.momentum,
+                            args.weight_decay,
+                            args.stat_decay,
+                            args.damping,
+                            args.kl_clip,
+                            args.TCov,
+                            args.TInv))
+elif optim_name == 'ekfac':
+    log_dir = os.path.join(args.log_dir, args.dataset, args.network + args_depth, args.optimizer,
+                           'bs%d_lr%.2f_mom%.2f_wd%.4f_sd%.4f_dmp%.4f_kl%.4f_TCov%d_TInv%d_TScal%d' %
+                           (args.batch_size,
+                            args.learning_rate,
+                            args.momentum,
+                            args.weight_decay,
+                            args.stat_decay,
+                            args.damping,
+                            args.kl_clip,
+                            args.TCov,
+                            args.TInv,
+                            args.TScal))
+
 if not os.path.isdir(log_dir):
     os.makedirs(log_dir)
 writer = SummaryWriter(log_dir)
