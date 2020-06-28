a79fc9e60e8e4adc4a746848edebe5f160e8a91c
ntselepidis <ntselepidis@student.ethz.ch>
2020-06-28 19:47:24 +0200

    Fixed bug in LR scheduler.

--- utils/lr_scheduler_utils.py
+++ utils/lr_scheduler_utils.py
@@ -8,12 +8,12 @@ def get_lr_scheduler(optimizer, args):
                                          patience=args.lr_sched_patience,
                                          min_lr=1e-7)
     elif args.lr_sched == 'multistep':
-        if args.milestone is None:
+        if args.lr_sched_milestone is None:
             lr_scheduler = MultiStepLR(optimizer,
                                        milestones=[int(args.epoch*0.5), int(args.epoch*0.75)],
                                        gamma=0.1)
         else:
-            milestone = [int(_) for _ in args.milestone.split(',')]
+            milestone = [int(_) for _ in args.lr_sched_milestone.split(',')]
             lr_scheduler = MultiStepLR(optimizer, milestones=milestone, gamma=0.1)
     else:
         raise NotImplementedError
