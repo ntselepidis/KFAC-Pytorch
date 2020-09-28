Fixed bug in computation of inter-layer covariances.

optimizers/gkfac.py
@@ -69,31 +69,22 @@ class GKFACOptimizer(optim.Optimizer):
         self.TInv = TInv
 
     @staticmethod
-    def _downsample_multiply(a, i, j, batch_size, module=None, batch_averaged=True): # module should be none for As and set for Gs
+    def _downsample_multiply(a, i, j, batch_size):
         # Get spatial dimensions of a[i] and a[j]
         spatial_dim_i = int(math.sqrt( a[i].shape[0] / batch_size ))
         spatial_dim_j = int(math.sqrt( a[j].shape[0] / batch_size ))
         if (spatial_dim_i == spatial_dim_j):
-            if isinstance(module, torch.nn.Linear) and batch_averaged:
-                cov_ij = a[i].t() @ (a[j] * batch_size)
-            else:
-                cov_ij = a[i].t() @ (a[j] / batch_size)
+            cov_ij = a[i].t() @ (a[j] / batch_size)
         elif (spatial_dim_j > spatial_dim_i):
             a_j_dsmpl = a[j].view(batch_size, spatial_dim_j, spatial_dim_j, -1).permute(0, 3, 1, 2)
             a_j_dsmpl = torch.nn.functional.interpolate(a_j_dsmpl, (spatial_dim_i, spatial_dim_i)).permute(0, 2, 3, 1)
             a_j_dsmpl = a_j_dsmpl.reshape(-1, a_j_dsmpl.size(-1))
-            if isinstance(module, torch.nn.Linear) and batch_averaged:
-                cov_ij = a[i].t() @ (a_j_dsmpl * batch_size)
-            else:
-                cov_ij = a[i].t() @ (a_j_dsmpl / batch_size)
+            cov_ij = a[i].t() @ (a_j_dsmpl / batch_size)
         else:
             a_i_dsmpl = a[i].view(batch_size, spatial_dim_i, spatial_dim_i, -1).permute(0, 3, 1, 2)
             a_i_dsmpl = torch.nn.functional.interpolate(a_i_dsmpl, (spatial_dim_j, spatial_dim_j)).permute(0, 2, 3, 1)
             a_i_dsmpl = a_i_dsmpl.reshape(-1, a_i_dsmpl.size(-1))
-            if isinstance(module, torch.nn.Linear) and batch_averaged:
-                cov_ij = a_i_dsmpl.t() @ (a[j] * batch_size)
-            else:
-                cov_ij = a_i_dsmpl.t() @ (a[j] / batch_size)
+            cov_ij = a_i_dsmpl.t() @ (a[j] / batch_size)
         return cov_ij
 
     def _save_input(self, module, input):
@@ -129,7 +120,7 @@ class GKFACOptimizer(optim.Optimizer):
             # Update off-diagonal blocks of G
             for j in range(i, self.nlayers):
                 # Compute inter-layer covariances (downsample if needed)
-                new_gg = self._downsample_multiply(self.g, i, j, grad_output[0].shape[0], module, self.batch_averaged)
+                new_gg = self._downsample_multiply(self.g, i, j, grad_output[0].shape[0])
                 # Initialize buffer
                 if self.steps == 0:
                     self.all_gg[i][j] = torch.zeros(self.g[i].shape[1], self.g[j].shape[1], device=new_gg.device)